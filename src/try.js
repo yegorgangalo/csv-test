3d3d70c83e75ab8be37f4fd7ebf91cb1bd694b79
Yegor Gangalo <gangaloyegor@gmail.com>
2021-03-11 01:21:01 +0200

    fix del

--- src/try.js
+++
@@ -1,8 +0,0 @@
-async function fetchData() {
-      const response = await fetch('../layers.csv');
-      const data = await response.text();
-      console.log(data);
-    }
-    fetchData();
-
-// fetch('./layers.csv').then(res=>res.text()).then(console.log)
